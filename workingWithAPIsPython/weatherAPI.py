1a3f9955dd8085941db7899a9fee0ad1051b0899
Shokran Ataii <ataiishokran@gmail.com>
2025-08-03 13:36:58 +0500

    weatherAPI.py

--- workingWithAPIsPython/weatherAPI.py
+++ workingWithAPIsPython/weatherAPI.py
@@ -2,16 +2,13 @@ import requests
 from PIL import Image
 from io import BytesIO
 base_url = 'http://api.weatherapi.com/v1'
-# create an account at the weather api website to otain an api key then copy and paste your key below.
+# create an account at www.weatherapi.com website to obtain an api key then copy and paste your key below.
 key = 'enter your key here'
 def current_weather(city):
     response = requests.get(f'{base_url}/current.json?key={key}&q={city}')
     if response.status_code == 200:
         weather_data = response.json()
-        # if you wanted to display the weather icon as well you can do this ut unfortunately you cannot display images in the terminal so if created a program with a gui someday you might use this.
-        #image_url = f"https:{weather_data['current']['condition']['icon']}"
-        #image_response = requests.get(image_url)
-        #image = Image.open(BytesIO(image_response.content))
+
         print(f'city: {weather_data['location']['name']}')
         print(f'Country: {weather_data['location']['country']}')
         print(f'Latitude: {weather_data['location']['lat']}')
@@ -25,5 +22,6 @@ def current_weather(city):
         return 'failed to retrieve data. Please get new api key of check your internet connection.'
 city = input('Enter the name of the city: ')
 weather_data = current_weather(city)
+
 
 
